329442dcec7d0ea3770ae4f23875e1d1601b23c8
Langa2007 <fidellanga67@gmail.com>
2026-08-03 18:05:19 +0300

    /

--- frontend/components/RongoApartmentAd.js
+++ frontend/components/RongoApartmentAd.js
@@ -34,7 +34,7 @@ export default function RongoApartmentAd({ advert }) {
             </div>
           ) : (
             <div className="italic p-4 text-sm leading-6 text-cream/80">
-              Rongo Furnished Apartment
+              Rongo Furnished Apartments
             </div>
           )}
         </div>
